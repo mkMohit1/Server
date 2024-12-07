7ba57b94721854bddecb0786fd02f43766346ffa
mkMohit1 <mohityoga.2016@gmail.com>
2024-12-07 17:45:14 +0530

    i have update the blog-model

--- models/blog-model.js
+++ models/blog-model.js
@@ -17,14 +17,22 @@ const blogSchema = new mongoose.Schema({
     type: String,
     required: true
    },
-   catergory:{
+   category:{
     type: String,
     required: true
    },
    date: {
     type: Date, // Use the Date type to store both date and time
     default: Date.now // Automatically set the current date and time by default
- } 
+ },
+ userImage:{
+    type: String,
+    default:'https://cdn-icons-png.flaticon.com/128/2202/2202112.png'
+ },
+ author: {
+    type: String,
+    default: "Admin"
+ },
 });
 
 const Blog = mongoose.model("Blog", blogSchema);
