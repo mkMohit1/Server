8a8a7960a8794bf4644856b5dc737fbf9907a5bb
mkMohit1 <mohityoga.2016@gmail.com>
2024-12-21 14:35:45 +0530

    i have update the db.js

--- utils/db.js
+++ utils/db.js
@@ -6,8 +6,9 @@ const URI = process.env.MONGODB_URI;
 
 const connectDb =async ()=>{
     try {
-        await mongoose.connect(URI);
-        console.error("database connection sucessfuly");
+        await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Database connection successful!");
+        return mongoose.connection.db; // Return the native MongoDB database connection
     } catch (error) {
         console.error("database connection failled");
         process.exit(0);
