1590156a08c1ee55328ca84130359011dfa75a0d
mkMohit1 <mohityoga.2016@gmail.com>
2025-02-19 11:17:30 +0530

    update core

--- Server.js
+++ Server.js
@@ -50,7 +50,7 @@ initializeRedis();
 
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-    ? 'https://cadabra.world/'  // Production frontend URL
+    ? 'https://cadabra.world'  // Production frontend URL
     : 'http://localhost:3000',
     credentials: true,              // Allow cookies and credentials
   }));
